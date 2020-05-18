18a17708cfb33d3dd451b498bee850b8f64aa184
AlexeyRakitin <amrakitin@gmail.com>
2020-05-18 10:45:46 +0300

    login with methods

--- bookmark/account/views.py
+++ bookmark/account/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def user_login(request):
+    # проверяем пост запрос, if request 'post'
+    if request.method == 'POST':
+        # создаем экземпляр формы с данным из post запроса
+        form = LoginForm(request.POST)
+        # проверяем валидность формы,  если не валидна в шаблоне отображаются ошибки формы
+        if form.is_valid():
+            # с помощью метода cleaned_data забираем данные из формы
+            cln_data = form.cleaned_data
+            # метод authenticate() проверяет данные в БД и возвращает нам объект User
+            user = authenticate(username=cln_data['username'], password=cln_data['passwords'])
+            # проверяем есть ли указнные пользователь
+            if user is not None:
+                # проверяем активен ли пользователь в БД
+                if user.is_active:
+                    # c помощью метода login входим в систему,
+                    # устанавливаем сессию (session) и возвращаем ответ об удачном входе
+                    login(request, user)
+                    return HttpResponse('Successfully authenticated!')
+                else:
+                    # если is_active = False, отвечаем что данный аккаунт не активен
+                    return HttpResponse('Login failed, account is disabled!')
+            else:
+                # если user = None - нет данного юзера в бд
+                return HttpResponse('Login failed, user does not exist!')
+    else:
+        # Если приходит post запрос GET, отрисовываем пустю форму
+        form = LoginForm()
+    # отображаем нашу Логин страницу login.html, передаем форму form
+    return render(request, 'account/login.html', {'form': form})
